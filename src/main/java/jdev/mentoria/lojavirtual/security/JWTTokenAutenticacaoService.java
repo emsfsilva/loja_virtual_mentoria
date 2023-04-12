5ea6ef97105e038bf74c598452af28a3cf97cade
emsfs <emsfs@Emerson>
2023-04-12 10:43:18 -0300

    Implemantação da camada de excecoes

--- src/main/java/jdev/mentoria/lojavirtual/security/JWTTokenAutenticacaoService.java
+++ src/main/java/jdev/mentoria/lojavirtual/security/JWTTokenAutenticacaoService.java
@@ -1,14 +1,23 @@
 package jdev.mentoria.lojavirtual.security;
 
+import java.io.IOException;
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import jdev.mentoria.lojavirtual.ApplicationContextLoad;
+import jdev.mentoria.lojavirtual.model.Usuario;
+import jdev.mentoria.lojavirtual.repository.UsuarioRepository;
 
 /*Cria a autenticação e retorna a autenticação*/
 
@@ -27,27 +36,83 @@ public class JWTTokenAutenticacaoService {
 	private static final String HEADER_STRING = "Authorization";
 
 	public void addAuthentication(HttpServletResponse response, String useername) throws Exception {
-		
-		/* Montagem do token*/
-		
-		String JWT = Jwts.builder()
-				.setSubject(useername)
-				.setExpiration(new Date(System.currentTimeMillis()+ EXPIRATION_TIME))
+
+		/* Montagem do token */
+
+		String JWT = Jwts.builder().setSubject(useername)
+				.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
 				.signWith(SignatureAlgorithm.HS512, SECRET).compact();
-		
+
 		String token = TOKEN_PREFIX + " " + JWT;
-		
+
 		response.addHeader(HEADER_STRING, token);
-		
+
+		liberacaoCors(response);
+
 		response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
+	/*
+	 * Metodo que retorna o usuario validado com o token ou caso nao seja valido
+	 * retorna null
+	 */
+	public Authentication getAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+
+		String token = request.getHeader(HEADER_STRING);
+
+		try {
+
+			if (token != null) {
+
+				String tokenLimpo = token.replace(TOKEN_PREFIX, "").trim();
+
+				String user = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(tokenLimpo).getBody().getSubject();
+
+				if (user != null) {
+					Usuario usuario = ApplicationContextLoad.getApplicationContext().getBean(UsuarioRepository.class)
+							.findUserByLogin(user);
+
+					if (usuario != null) {
+						return new UsernamePasswordAuthenticationToken(usuario.getLogin(), usuario.getSenha(),
+								usuario.getAuthorities());
+
+					}
+				}
+			}
+
+		} catch (SignatureException e) {
+			response.getWriter().write("Token está invalido");
+
+		} catch (ExpiredJwtException e) {
+			response.getWriter().write("Token está expirado. Efetue login Novamente");
+
+		}
+		finally {
+			liberacaoCors(response);
+		}
+
+		liberacaoCors(response);
+		return null;
+	}
+
+	private void liberacaoCors(HttpServletResponse response) {
+
+		if (response.getHeader("Access-Control-Allow-Origin") == null) {
+			response.addHeader("Access-Control-Allow-Origin", "*");
+		}
+
+		if (response.getHeader("Access-Control-Allow-Headers") == null) {
+			response.addHeader("Access-Control-Allow-Headers", "*");
+		}
+
+		if (response.getHeader("Access-Control-Request-Headers") == null) {
+			response.addHeader("Access-Control-Request-Headers", "*");
+		}
+
+		if (response.getHeader("Access-Control-Allow-Methods") == null) {
+			response.addHeader("Access-Control-Allow-Methods", "*");
+		}
+	}
+
 }
--- src/main/java/jdev/mentoria/lojavirtual/security/WebConfigSecurity.java
+++ src/main/java/jdev/mentoria/lojavirtual/security/WebConfigSecurity.java
@@ -7,10 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import jdev.mentoria.lojavirtual.service.ImplementacaoUserDetailsService;
 
@@ -18,25 +22,32 @@ import jdev.mentoria.lojavirtual.service.ImplementacaoUserDetailsService;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements HttpSessionListener {
-	
+
 	@Autowired
 	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
-	
-	/* Irá consultar o usuario no banco de dados com Spring Security*/
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).disable().authorizeRequests()
+				.antMatchers("/").permitAll().antMatchers("/index").permitAll().antMatchers(HttpMethod.OPTIONS, "/**")
+				.permitAll().anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).and()
+				.addFilterAfter(new JWTLoginFilter("/login", authenticationManager()),
+						UsernamePasswordAuthenticationFilter.class)
+				.addFilterBefore(new JwtApiAutenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
+
+	/* Irá consultar o usuario no banco de dados com Spring Security */
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(implementacaoUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-		
-	
-	
+
 	}
-	
-	
-	
+
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso", "/deleteAcesso")
-		.antMatchers(HttpMethod.POST, "/salvarAcesso", "/deleteAcesso");
+		/*web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso", "/deleteAcesso").antMatchers(HttpMethod.POST,
+				"/salvarAcesso", "/deleteAcesso");*/
 	}
 
 }
